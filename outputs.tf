2ae3c559bb0578f5594a84bb717fbc137bb1b266
Craig Phillips <cphillips@2u.com>
2021-02-24 10:39:29 -0500

    Workflow Comment Output - 02 - fixed files

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-   output "all" {
-        value = data.aws_caller_identity.current
-         }
+output "all" {
+  value = data.aws_caller_identity.current
+}
 
-          output "yup" {
-                         value = data.aws_caller_identity
-                          }
+output "yup" {
+  value = data.aws_caller_identity
+}
